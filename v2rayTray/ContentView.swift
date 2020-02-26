f7f78cf8ba40109ce637211dcfdb188638d0bde8
dreamldx <dreamldx@gmail.com>
2020-02-26 18:58:47 +0800

    Initial Commit

---
+++ v2rayTray/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  v2rayTray
+//
+//  Created by dreamldx on 2020/2/26.
+//  Copyright © 2020 dreamldx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
