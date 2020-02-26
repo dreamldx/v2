21804aeeee379b73e7c43ffcb08b8b30c1301dec
dreamldx <dreamldx@gmail.com>
2020-02-26 20:53:16 +0800

    Initial Check-in.

--- v2rayTray/AppDelegate.swift
+++ v2rayTray/AppDelegate.swift
@@ -12,28 +12,46 @@ import SwiftUI
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
+    var item: NSStatusItem!
+    var v2ray: Process!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Create the window and set the content view. 
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        v2ray = Process()
+        v2ray.executableURL =  Bundle.main.url(forResource: "v2ray", withExtension: "")
+        let url = Bundle.main.url(forResource: "config", withExtension: "json")
+        v2ray.arguments = ["-config", url!.path as String]
+        
+        do {
+        try v2ray.run()
+        } catch {
+                
+        }
+        
+        let bar = NSStatusBar.system
+        item = bar.statusItem(withLength: NSStatusItem.squareLength)
+        item.button?.title = "V2"
+        
+        let menu = NSMenu(title: "Context")
+        item.menu = menu
+        
+        menu.addItem(
+            withTitle: "Exit",
+            action: #selector(AppDelegate.exitApplication),
+            keyEquivalent: ""
+        )
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        v2ray.terminate()
     }
-
-
+    
+    @objc
+    func exitApplication() {
+        v2ray.terminate()
+        exit(0)
+    }
+    
 }
 
